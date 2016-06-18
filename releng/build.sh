7c3eeb468bfd1000d71b3e86ec47e36348bb4991
raingloom <raingloom42@gmail.com>
2016-06-18 21:29:23 +0200

    Revert "patched multilib into build.sh"
    This reverts commit 656999ff2dbc0e8e1b5f51a5c15473276bbe50c2.

--- releng/build.sh
+++ releng/build.sh
@@ -246,26 +246,19 @@ done
 
 mkdir -p ${work_dir}
 
-cp -v pacmanx86_64.conf pacman.conf
 run_once make_pacman_conf
 
 # Do all stuff for each airootfs
-for arch in x86_64; do
+for arch in i686 x86_64; do
     run_once make_basefs
     run_once make_packages
-    run_once make_packages_efi
-    run_once make_setup_mkinitcpio
-    run_once make_customize_airootfs
 done
 
 echo make_pacman_conf i686
 cp -v pacmani686.conf pacman.conf
 cp -v pacmani686.conf ${work_dir}/pacman.conf
 
-for arch in i686; do
-    run_once make_setup_mkinitcpio
-    run_once make_customize_airootfs
-    run_once make_packages_efi
+for arch in i686 x86_64; do
     run_once make_setup_mkinitcpio
     run_once make_customize_airootfs
 done
