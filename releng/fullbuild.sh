a1c6df62be0eba1eaa2a1af87446a1bf9949b6c0
raingloom <raingloom42@gmail.com>
2016-09-24 11:17:46 +0200

    fixed the pkgcache build order

--- releng/fullbuild.sh
+++ releng/fullbuild.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd `dirname $0`
 cd pkgcache
-make PACARG="--noedit --noconfirm" dirs symlink dbclear pkgclear dbupdate base base-devel bootloader packages.both store
+make PACARG="--noedit --noconfirm" dbclear pkgclear dirs store symlink dbupdate base base-devel bootloader packages.both store
 cd ..
 sudo ./overlay.sh
 sudo ./rebuild.sh $@
